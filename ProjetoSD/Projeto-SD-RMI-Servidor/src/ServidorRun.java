36a99f33a880b8f1da3e82f0cfb5902769781741
davidson.or@gmail.com <davidson.or@gmail.com@9413eb5d-c298-562d-a774-3c8ba49cc708>
2012-12-01 18:14:59 +0000

    criando servidor run

--- ProjetoSD/Projeto-SD-RMI-Servidor/src/ServidorRun.java
+++ ProjetoSD/Projeto-SD-RMI-Servidor/src/ServidorRun.java
@@ -6,24 +6,65 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-
 public class ServidorRun {
+
+	private static final String URL_REGISTER_SERVICE = "rmi://localhost/registro";
+	
+	private static final String URL_REPLICACAO_SERVICE = "rmi://localhost/replicacao";
+
+	public ServidorRun() {
+		this.startRegisterService();
+		this.startReplicaoService();
+	}
 
 	/**
 	 * @param args
-	 * @throws RemoteException 
+	 * @throws RemoteException
 	 */
 	public static void main(String[] args) throws RemoteException {
-		InterfaceRegistro replicacao = null;
-		try {
-			replicacao = (InterfaceRegistro) Naming.lookup("rmi://localhost/registro");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		
-		replicacao.registraServidor(ToolsHelp.catchIpMachine());
-		
+		new ServidorRun(); 
 	}
 
-	
+	private void startReplicaoService() {
+		try {
+			ReplicacaoImpl replicacaoImpl = new ReplicacaoImpl();
+			System.out.println("Disponibilizando serviço de replicação....");
+			Naming.rebind(URL_REPLICACAO_SERVICE, replicacaoImpl);
+			System.out.println("Serviço Disponivel");
+		} catch (RemoteException e) {
+			System.out.println("Erro de Conexão.");
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			System.out.println("Endereço incorreto ou mal formado.");
+			e.printStackTrace();
+		}
+	}
+
+	private void startRegisterService() {
+		InterfaceRegistro replicacao = null;
+		try {
+			System.out.println("Iniciando serviço de registro de servidor....");
+			replicacao = (InterfaceRegistro) Naming.lookup(URL_REGISTER_SERVICE);
+			System.out.println("Serviço iniciado com sucesso!");
+		} catch (RemoteException e) {
+			System.out.println("Erro de Conexão.");
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			System.out.println("Endereço incorreto ou mal formado.");
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			System.out.println("Não foi possível criar conexão com servidor no endereço: " + URL_REGISTER_SERVICE);
+			e.printStackTrace();
+		}
+
+		System.out.println("Solicitando registro com ip: " + ToolsHelp.catchIpMachine() + "...");
+		try {
+			replicacao.registraServidor(ToolsHelp.catchIpMachine());
+			System.out.println("Ip: " + ToolsHelp.catchIpMachine() + "registrado com sucesso!");
+		} catch (RemoteException e) {
+			System.out.println("Erro de Conexão ao registrar ip: " + ToolsHelp.catchIpMachine());
+			e.printStackTrace();
+		}
+	}
+
 }
