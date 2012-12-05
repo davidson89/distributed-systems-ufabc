491213712057e184e5256ec5dc9a730bf364f7d7
rafa_metallica@hotmail.com <rafa_metallica@hotmail.com@9413eb5d-c298-562d-a774-3c8ba49cc708>
2012-12-05 00:26:43 +0000

    Método apagar e classe Arquivo

--- trunk/ProjetoSD/Projeto-SD-RMI-Controlador/src/ControladorImpl.java
+++ trunk/ProjetoSD/Projeto-SD-RMI-Controlador/src/ControladorImpl.java
@@ -12,17 +12,16 @@ import exceptions.NenhumServidorDisponivelException;
 import exceptions.ObjetoExistenteException;
 import exceptions.ObjetoNaoEncontradoException;
 
-
 public class ControladorImpl extends UnicastRemoteObject implements InterfaceControlador {
 
 	private ObjectContainerAndServers instance;
-	
+
 	private InterfaceReplicacao interfaceReplicao;
-	
+
 	private AdministratorServers admServers;
-	
+
 	private AdministratorObjects admObjects;
-	
+
 	protected ControladorImpl() throws RemoteException {
 		super();
 		this.instance = ObjectContainerAndServers.getInstance();
@@ -33,30 +32,49 @@ public class ControladorImpl extends UnicastRemoteObject implements InterfaceCon
 	@Override
 	public void apaga(String key) throws RemoteException,
 			NenhumServidorDisponivelException, ObjetoNaoEncontradoException {
-		
+		if (this.instance.getMapKeyId().containsKey(key)) {
+			int id = this.instance.getMapKeyId().get(key).intValue();
+			// remover dos servidores (todos)
+			for (String servidor : this.instance.getTodosServidores()) {
+				try {
+					this.interfaceReplicao = (InterfaceReplicacao) Naming
+							.lookup("rmi://" + servidor + "/replicacao");
+				} catch (MalformedURLException e) {
+					System.out.println("Servidor não encontrado: " + servidor);
+					this.admServers.excluirServidor(servidor);
+				} catch (NotBoundException e) {
+					e.printStackTrace();
+				}
+				this.interfaceReplicao.apaga(id);
+			}
+			// remover dos mapas (os 2)
+			this.instance.removeObjeto(key);
+		} else {
+			throw new ObjetoNaoEncontradoException(key);
+		}
 	}
 
 	@Override
 	public void armazena(String key, Object objeto) throws RemoteException,
-			NenhumServidorDisponivelException,  ObjetoExistenteException{
-		if(this.instance.getMapKeyId().containsKey(key)) {
+			NenhumServidorDisponivelException, ObjetoExistenteException {
+		if (this.instance.getMapKeyId().containsKey(key)) {
 			throw new ObjetoExistenteException(key);
 		}
-		if(this.instance.getTodosServidores().isEmpty()) {
+		if (this.instance.getTodosServidores().isEmpty()) {
 			throw new NenhumServidorDisponivelException();
 		}
 
 		int id = this.instance.guardaObjeto(key, objeto);
-		for(String servidor : this.instance.getTodosServidores()) {
+		for (String servidor : this.instance.getTodosServidores()) {
 			try {
-				this.interfaceReplicao = (InterfaceReplicacao) Naming.lookup("rmi://"+ servidor +"/replicacao");
+				this.interfaceReplicao = (InterfaceReplicacao) Naming.lookup("rmi://" + servidor + "/replicacao");
 			} catch (MalformedURLException e) {
 				System.out.println("Servidor não encontrado: " + servidor);
 				this.admServers.excluirServidor(servidor);
 			} catch (NotBoundException e) {
 				e.printStackTrace();
 			}
-			
+
 			this.interfaceReplicao.replica(id, objeto);
 		}
 	}
@@ -70,16 +88,14 @@ public class ControladorImpl extends UnicastRemoteObject implements InterfaceCon
 	public String procura(String key) throws RemoteException,
 			ObjetoNaoEncontradoException, NenhumServidorDisponivelException {
 		int identificador = this.admObjects.verificaExistenciaObjeto(key);
-		
+
 		String link = String.valueOf(identificador);
 		link = link.concat("@rmi://");
 		String serverDisp = admServers.getServerDisp();
 		link = link.concat(admServers.getURLAcesso(serverDisp));
 		link = link.concat("/acesso");
-		
+
 		return link;
 	}
-
-
 
 }
