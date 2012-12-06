33f4d3b8ee39180d121c4355c1ceab43b7122cc5
davidson.or@gmail.com <davidson.or@gmail.com@9413eb5d-c298-562d-a774-3c8ba49cc708>
2012-12-06 00:09:07 +0000

    Arrumando tamanho do botão e listar

--- trunk/ProjetoSD/Projeto-SD-RMI-Cliente/src/cliente/Main.java
+++ trunk/ProjetoSD/Projeto-SD-RMI-Cliente/src/cliente/Main.java
@@ -9,39 +9,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
 	         TelaCliente t = new TelaCliente();
 	           t.setVisible(true);
-		/*
-		try {
-			cliente.armazena("teste", new String("FF"));
-		} catch (ObjetoExistenteException e) {
-			System.out.println("Objeto ja cadastrado");
-		}
-		espera(3000);
-		String t = cliente.procura("teste");
-		System.out.println("Endereço: " + t);
-		
-		System.out.println("Solicitando recuperação");
-		Arquivo myFile = (Arquivo) cliente.recupera(t);
-		
-
-		System.out.println("Solicitando lista de objetos para o controlador...");
-		String[] lista = (String[]) cliente.lista();
-		System.out.println("Lista solicitada com sucesso!");
-
-		System.out.println("Objetos armazenados no servidor: ");
-
-		if (lista.length == 0) {
-			System.out.println("Não existe objetos armazenados!");
-		}
-
-		for (int i = 0; i < lista.length; i++) {
-			System.out.println(lista[i]);
-		}
-*/
-		
-
 	}
 
 	public static void espera(int tempo) {
